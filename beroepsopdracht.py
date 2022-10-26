57134e9e846d2412bdffa9e9a238cc547814db51
Seinaru <34807@ma-web.nl>
2022-10-26 10:37:03 +0200

    python

---
+++ beroepsopdracht.py
@@ -0,0 +1,225 @@
+from time import sleep
+def plot1():
+    print("This story begins with a man named Clover Taurus")
+    sleep(1)
+    print("Hes not the biggest known man, but hes still quite popular")
+    sleep(1)
+
+def plot2():
+    print("")
+
+def plot3():
+    print("")
+
+def plot4():
+    print("")
+
+def plot5():
+    print("")
+
+def Plot6():
+    print("")
+
+
+
+def Plot7():
+    print("")
+
+def plot8():
+    print("")
+
+def plot9():
+    print("")
+
+def plot10():
+    print("")
+
+def plot11():
+    print("")
+
+def plot12():
+    print("")
+
+def plot13():
+    print("")
+
+def plot14():
+    print("")
+
+
+
+def plot15():
+    print("")
+
+def plot16():
+    print("")
+
+def plot17():
+    print("")
+
+def plot18():
+    print("")
+    
+def plot19():
+    print("")
+
+def plot21(): 
+    print("")
+
+def plot22(): 
+    print("")
+
+def plot23(): 
+    print("")
+
+def plot24(): 
+    print("")
+
+def plot25(): 
+    print("")
+
+def plot26(): 
+    print("")
+
+def plot27(): 
+    print("")
+
+def plot28(): 
+    print("")
+
+def plot29(): 
+    print("")
+
+
+
+def plot30(): 
+    print("")
+
+def plot31(): 
+    print("")
+
+def plot32(): 
+    print("")
+
+def plot33(): 
+    print("")
+
+def plot34(): 
+    print("")
+
+def plot35(): 
+    print("")
+
+def plot36(): 
+    print("")
+
+def plot37(): 
+    print("")
+
+def plot38(): 
+    print("")
+
+def plot39(): 
+    print("")
+
+def plot40(): 
+    print("")
+
+def plot41(): 
+    print("")
+
+def plot42(): 
+    print("")
+
+def plot43(): 
+    print("")
+
+def plot44(): 
+    print("")
+
+def plot45(): 
+    print("")
+
+def plot46(): 
+    print("")
+
+def plot47(): 
+    print("")
+
+
+
+def plot48(): 
+    print("")
+
+def plot49(): 
+    print("")
+
+
+
+def plot50(): 
+    print("")
+
+def plot51(): 
+    print("")
+
+def plot52(): 
+    print("")
+
+def plot53(): 
+    print("")
+
+def plot54(): 
+    print("")
+
+def plot55(): 
+    print("")
+
+def plot56(): 
+    print("")
+
+def plot57(): 
+    print("")
+
+def plot58(): 
+    print("")
+
+def plot59(): 
+    print("")
+
+def plot60(): 
+    print("")
+
+def plot61(): 
+    print("")
+
+def plot62(): 
+    print("")
+
+def plot63(): 
+    print("")
+
+def plot64(): 
+    print("")
+
+def plot65(): 
+    print("")
+
+def plot66(): 
+    print("")
+
+
+
+def eind1(): 
+    print("")
+
+def eind2(): 
+    print("")
+
+def eind3(): 
+    print("")
+
+def eind4(): 
+    print("")
+
+def eind5(): 
+    print("")
