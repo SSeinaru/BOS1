0c3f99ed7f23bb854637dc679034e613b8808bc9
Seinaru <34807@ma-web.nl>
2022-10-29 20:48:26 +0200

    A

--- beroepsopdracht.py
+++ beroepsopdracht.py
@@ -4,18 +4,73 @@ def plot1():
     sleep(1)
     print("Hes not the biggest known man, but hes still quite popular")
     sleep(1)
+    print("Ofcourse this isn't just some random ass boring story")
+    sleep(1)
+    print("because you are the one who makes the choices")
+    sleep(1)
+    print("some background info about Clover")
+    sleep(1)
+    print("Hes a B-rank Adventurer, He just kinda does this for the funny xd lmao")
+    sleep(1)
+    print("Now that that is said lets begin")
+    plot2
 
 def plot2():
-    print("")
+    print("You wake up on your tatami mat")
+    sleep(1)
+    print("you get out of bed and get dressed")
+    sleep(1)
+    print("You look at your room and then you look at the others rooms")
+    sleep(1)
+    print("You finally realise how dirty your house")
+    sleep(1)
+    print("You decide to maybe do it later, because after all")
+    sleep(1)
+    print("because breakfast is more important")
+    sleep(1)
+    print("you look into your look around if there is anything to eat")
+    sleep(1)
+    print("you find some gold coins and some sushi rice and some fish")
+    sleep(1)
+    print("So you have 2 options, you use the coins to get some fresh bread")
+    sleep(1)
+    print("Or you make some sushi")
+    sleep(1)
+    print("So what do you decide eat? A. Buy bread B. Make sushi")
+    food = input().lower()
+
+    if food == "a":
+        plot3
+    
+    elif food == "b":
+        plot4
 
 def plot3():
-    print("")
+    print("So you decide to go buy some bread")
+    sleep(1)
+    print("You go to the bakery")
+    sleep(1)
+    print("Inside the bakery you meet the baker and see that he has 2 loafs of bread left")
+    sleep(1)
+    print("you ask him if he has any bread left for sale")
+    sleep(1)
+    print("He answers that he has a singular loaf left for sale")
+    sleep(1)
+    print("All the others have been sold and the other one is on reservation")
+    sleep(1)
+    print("thank fully you walk back with the bread piece")
+    sleep(1)
+    print("when you arrive home you cut the bread and make some sandviches")
+    sleep(1)
+    print("you thought that you might need some later so you make some extra's")
+    sleep(1)
+    plot5
 
 def plot4():
     print("")
 
 def plot5():
-    print("")
+    print("After that you get ready to head out")
 
 def Plot6():
     print("")
